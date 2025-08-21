a82eece484a1c62096d8e0f4f6ca495fc1d8c750
Ayan Kumar Panja <ayanpanja1992@gmail.com>
2025-08-22 01:57:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -50,6 +50,4 @@ def predict():
         return render_template("index.html", prediction_value=f"Error: {e}")
 
 if __name__ == "__main__":
-    # For local testing; on Render you'll use a start command like:
-    # gunicorn -w 2 -k gthread -b 0.0.0.0:$PORT app:app
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True)
